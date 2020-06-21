0fa4900a0d8198764be6acf0cd28b8e05df6bde6
bliutwo <bliutwo@cs.stanford.edu>
2020-06-21 14:04:41 -0700

    21/06/2020 14:04:40

--- get_hot_day_posts_from_subreddit.py
+++ get_hot_day_posts_from_subreddit.py
@@ -85,7 +85,7 @@ smmry = SmmryAPI(api_key)
 
 for submission in submissions:
     title = submission.title
-    if "Daily Discussion" not in title and "Looking For Participants" not in title and "Join our Official Discord" not in title and "For US Patients Diagnosed" not in title:
+    if "Daily Discussion" not in title and "Looking For Participants" not in title and "Join our Official Discord" not in title and "For US Patients Diagnosed" not in title and "Clinical study in Austin, TX" not in title:
         submission.comment_sort = "top"
         top_level_comments = list(submission.comments)
         top_comment = None
