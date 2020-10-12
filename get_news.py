29b9dff05959b3e36131a352a3864e9512fa65c6
bliutwo <bliutwo@cs.stanford.edu>
2020-10-11 21:07:22 -0700

    11/10/2020 21:07:22

--- get_news.py
+++ get_news.py
@@ -1,7 +1,7 @@
 import get_hot
 import threading
 
-l = ['coronavirus', 'news', 'coronavirusca', 'politics', 'worldnews']
+l = ['coronavirus', 'news', 'coronavirusca', 'politics', 'worldnews', 'science']
 threads = []
 for s in l:
     t = threading.Thread(target=get_hot.get_hot, args=(s,))
